d508d6be273c451e74df21f8e1cf0c0637900654
NgosaLwando <ngosalwando@example.com>
2025-08-05 17:19:00 +0200

    Update ALLOWED_HOSTS for clean domain

--- Dalisto/settings.py
+++ Dalisto/settings.py
@@ -26,7 +26,12 @@ SECRET_KEY = 'django-insecure-j!^0=0bvi-8&+7e(a%rz55tv+iy*p55xz9o3ne!m3==v573!4v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['dalisto-talk-finance.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = [
+'dalitso-talk-finance.herokuapp.com', 
+'localhost', 
+'127.0.0.1',
+]
+
 
 
 
