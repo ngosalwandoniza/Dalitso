48d5c0e1cccac326aadac620633639b33fe38c67
NgosaLwando <ngosalwando@example.com>
2025-08-05 15:49:28 +0200

    Initial commit for Heroku deployment

--- Dalisto/settings.py
+++ Dalisto/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-j!^0=0bvi-8&+7e(a%rz55tv+iy*p55xz9o3ne!m3==v573!4v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
